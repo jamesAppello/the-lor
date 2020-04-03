03143cde01ef5ab24a54528d216b9a8d1851a92c
jamesAppello <olleppasemaj@gmail.com>
2020-04-02 23:33:17 -0400

    thank you stack overflow

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 //NPM packages first || parent_imports
 // DOTENV FIRST FOR NO EVNIRONEMT_OVERLAPPING WITH OTHER PACKAGE IMPORTS
-require('dotenv').config();
-const axios = require('axios');
-const inquirer = require('inquirer');
-const fs = require('fs');
+require("dotenv").config();
+const axios = require("axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
 // child_imports || HELPERS
+// const { shuffle } = require('./helper');
 // CLI-ARGS
 const argOne = process.argv[2];
 const argTwo = process.argv[3];
-// console.log(`argOne: ${argOne}`);
-// console.log(`argTwo: ${argTwo}`);
 
-
-const alpha = require('alphavantage')({ key: process.env.ALPHAVANTAGE_API_KEY });
-//API DOCS: " Your api key will be viable in the network traffic, ths sould not be used for public projects "
-// console.log(alpha)
-
-// alpha.data.intraday('work').then(data => {
-    
-//     console.log(data);
-// })
+//API DOCS: " Your api key will be visable in the network traffic, ths sould not be used for public projects "
+const alpha = require("alphavantage")({
+  key: process.env.ALPHAVANTAGE_API_KEY
+});
 
 /**
  * GIVENS:
@@ -31,8 +25,10 @@ const alpha = require('alphavantage')({ key: process.env.ALPHAVANTAGE_API_KEY })
  * * ELSE do different outcomes for cli args *
  * * :|:?WHAT_THO?:|:(K.I.S.S)=> 3 actions    *
  * ==========================================
- * 
- *
+ * CHECKLIST:
+ * <get-stock-data> :: works ****** go back to make the displayed output look more pristine
+ * <hash-this-string>:: IN_PROGRESS << " so far I have the string query pushed into an empty array; then have split the querySTR into individual indexes "
+ * <ef-es>:: NOT_STARTED:: << deciding the best optimal approach to write/read/append-File method.....BECAUSE INQUIRER WILL HELP ME WITH MAKING THAT LOOK AMAING!
  * ==========================================
  * @-the MAIN:<:< function (action, query){
  * SWITCH<~>action
@@ -42,8 +38,8 @@ const alpha = require('alphavantage')({ key: process.env.ALPHAVANTAGE_API_KEY })
  * >>WORKS>>> 4|4/2/2020|18:06|>>>>>>>>|||||||||
  * ===================================================~~~~~~~~~~~~~~~
  *      2~>>> password-hasher-generator << "hash-this-string"
- * // take input string, 
- * generate input (n)  
+ * // take input string,
+ * generate input (n)
  * => return (n*phi) * (random number picked from range 0 to 1618)^e(Pi)
  * ....then cos(result) then cot(cos_res)
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -53,50 +49,52 @@ const alpha = require('alphavantage')({ key: process.env.ALPHAVANTAGE_API_KEY })
  * // append-file << add to present file >> 'list' || data
  * ========================================================
  * ELSE => just add the node spotify api too to whatever else i get to work as an EXTRA cherry on top...OMDB too while im at it.
- * } 
+ * }
  */
-
 
 // MAIN
 // I::>
 // define method
 const runApp = function(action, query) {
-    switch (action) {
-        case "get-stock-data":
-            console.log('STOCK_METHOD\n'+query);
-            alpha.data.intraday(query).then(stockData => {
-                console.log(stockData);
-            })
-            break;
+  switch (action) {
+    case "get-stock-data":
+      console.log("STOCK_METHOD\n" + query);
+      alpha.data.intraday(query).then(stockData => {
+        console.log(stockData);
+      });
+      break;
 
-        case "hash-this-string":
-            console.log('PASSWORD_GENERATOR_METHOD\n'+query);
-            let pwdArrHolder = [];
-            // take string....push into array
-            pwdArrHolder.push(query);
-            // advisory errorMessage to make users password use a more secure string!
-            console.log(pwdArrHolder);
-            if (query.length < 5) {
-                throw { message: 'IDEALLY 8+ CHARACTERS TO MAKE IT MORE SECURE!' }
-            }
-            console.log(query.split(''));
-            break;
+    case "hash-this-string":
+      console.log("PASSWORD_GENERATOR_METHOD\n" + query);
+       
+        var a = query.split(""),
+          n = a.length;
 
-        case "ef-es":
-            console.log('CRUD_FILE-SYSTEM_METHOD'); 
-            break;       
-    }
+        for (var i = n - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var tmp = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+          console.log(a.join(""));
+        // @ this point it prints out a different shuffle per iteration.....
+        // I think this is even better of an outcome than i expected!
+        }
+      break;
+
+    case "ef-es":
+      console.log("CRUD_FILE-SYSTEM_METHOD");
+      break;
+  }
 };
 
 // O::>
 // callback the main ~|.\./.|~
 runApp(argOne, argTwo);
 
-
 /**
- * 
+ *
  * WHEN THIS ALL RUNS PERFECTLY THEN TRANSFORM THIS USING THE INQUIRER PACKAGE
- * >>EXACTLY HOW I WISHED I HAD IT WORKING BACK WHEN IN RCB...IM GOING TO MAKE IT WORK NOW 
+ * >>EXACTLY HOW I WISHED I HAD IT WORKING BACK WHEN IN RCB...IM GOING TO MAKE IT WORK NOW
  * >>MORE CONFIDENCE IN MYSELF NOW THAN EVER BEFORE TO BE ABLE TO PULL THIS OFF!
- * 
+ *
  */
